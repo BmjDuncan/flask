3383756073f5243e21c69da33326c50e17e8dd33
BmjDuncan <114992961+BmjDuncan@users.noreply.github.com>
2022-12-06 14:55:46 +0000

    Update catalogue.py

--- catalogue.py
+++ catalogue.py
@@ -75,7 +75,7 @@ def cat_page():
               html=html+'<h3>'+name+'</h3>'
               ServerIP=request.host.split(':')[0]
               html=html+'<a href="http://'+ServerIP+'/Video/'+uuid+'">'
-              html=html+'<img src="http://35.228.145.155/pics/'+thumb+'">'
+              html=html+'<img src="http://34.140.11.120/pics/'+thumb+'">'
               html=html+"</a>"        
               print("=======================")
 
