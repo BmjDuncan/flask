2c5b964a983cc46bfc702fb8c0047bc9655723f5
BmjDuncan <114992961+BmjDuncan@users.noreply.github.com>
2023-01-09 15:58:07 +0000

    Update catalogue.py

--- catalogue.py
+++ catalogue.py
@@ -12,7 +12,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 app = Flask(__name__)
 app.debug = True
 
-cnx = mysql.connector.connect(user='root', password='dacjd156n.',host='0.0.0.0')
+cnx = mysql.connector.connect(user='root', password='dacjd156n.',host='some-mysql')
 cursor = cnx.cursor()
 create_database(cnx,cursor)
 
