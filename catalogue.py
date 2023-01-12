5161d798b788c7e9eec05be8bf34ada9800fdbcc
BmjDuncan <114992961+BmjDuncan@users.noreply.github.com>
2023-01-12 16:12:16 +0000

    Update catalogue.py

--- catalogue.py
+++ catalogue.py
@@ -125,7 +125,6 @@ def login():
 def logout():
     # Remove session data, this will log the user out
    session.pop('loggedin', None)
-   session.pop('id', None)
    session.pop('username', None)
    # Redirect to login page
    return redirect(url_for('login'))
