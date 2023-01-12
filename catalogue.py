b76b541e6f903f72ae6b5e4470cf6d35dc8ad789
BmjDuncan <114992961+BmjDuncan@users.noreply.github.com>
2023-01-12 16:01:13 +0000

    Update catalogue.py

--- catalogue.py
+++ catalogue.py
@@ -46,50 +46,52 @@ def video_page(video):
     return render_template('video.html', name=video,file=videofile,pic=pic)
 
 @app.route('/cat')
-def cat_page():
-    url = "http://34.76.74.49/myflix/videos"
-    url2="http://34.76.74.49/myflix/categories"
-    response2=requests.get(url2)
-    jResp2=response2.json()
+def cat_page():  
+    if 'loggedin' in session:
+        url = "http://34.76.74.49/myflix/videos"
+        url2="http://34.76.74.49/myflix/categories"
+        response2=requests.get(url2)
+        jResp2=response2.json()
 
-    headers = {}
-    payload = json.dumps({ })
+        headers = {}
+        payload = json.dumps({ })
 
-    response = requests.get(url)
-    #print (response)
-    # exit if status code is not ok
-    print (response)
-    print (response.status_code)
-    if response.status_code != 200:
-      print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
-      return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
-    jResp = response.json()
-    print (type(jResp))
-    html="<h1> Your Videos</h1>"
-    for cat in jResp2:
-        html=html+'<div style="display:flex">'
-        html=html+'<h2>'+cat["category"].title()+'</h2>'
-        for index in jResp:
-        #print (json.dumps(index))
-            #print ("----------------")
-            for key in index:
+        response = requests.get(url)
+        #print (response)
+        # exit if status code is not ok
+        print (response)
+        print (response.status_code)
+        if response.status_code != 200:
+          print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
+          return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
+        jResp = response.json()
+        print (type(jResp))
+        html="<h1> Your Videos</h1>"
+        for cat in jResp2:
+            html=html+'<div style="display:flex">'
+            html=html+'<h2>'+cat["category"].title()+'</h2>'
+            for index in jResp:
+            #print (json.dumps(index))
+                #print ("----------------")
+                for key in index:
 
-                if (key !="_id"):
-                    if cat["category"] in index[key]["category"]:
-                        name=index[key]["Name"]
-                        thumb=index[key]["thumb"]
-                        uuid=index[key]["uuid"]
-                        html=html+'<div style="margin-right:10px;">'
-                        html=html+'<p style="text-align:center;">'+name.title()+'<p>'
-                        ServerIP=request.host.split(':')[0]
-                        html=html+'<a href="http://'+ServerIP+'/Video/'+uuid+'">'
-                        html=html+'<img src="http://34.79.49.178/pics/'+thumb+'">'
-                        html=html+"</a>"
-                        html=html+'</div>'
-        html=html+'</div>'
-    return html
-
-@app.route('/', methods=['GET', 'POST'])
+                    if (key !="_id"):
+                        if cat["category"] in index[key]["category"]:
+                            name=index[key]["Name"]
+                            thumb=index[key]["thumb"]
+                            uuid=index[key]["uuid"]
+                            html=html+'<div style="margin-right:10px;">'
+                            html=html+'<p style="text-align:center;">'+name.title()+'<p>'
+                            ServerIP=request.host.split(':')[0]
+                            html=html+'<a href="http://'+ServerIP+'/Video/'+uuid+'">'
+                            html=html+'<img src="http://34.79.49.178/pics/'+thumb+'">'
+                            html=html+"</a>"
+                            html=html+'</div>'
+            html=html+'</div>'
+        return html
+    return redirect(url_for('login'))
+    
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     url = "http://35.195.173.196/myflix/users"
     response=requests.get(url)
@@ -112,12 +114,22 @@ def login():
             session['loggedin'] = True
             session['username'] = account['username']
             # Redirect to home page
-            return 'Logged in successfully!'
+            return redirect(url_for('cat'))
         except:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
+
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port="80")
